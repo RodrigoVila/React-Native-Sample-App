2fcadfbbe605458829aef2ff934d87e7be7a2ea9
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2020-01-11 16:08:46 -0300

    Game ScreenAdded

--- Components/GTN/StartGameScreen.js
+++ Components/GTN/StartGameScreen.js
@@ -1,33 +1,60 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  TextInput,
   Button,
   TouchableWithoutFeedback,
   Keyboard,
   Alert
-} from "react-native";
+} from 'react-native';
 
+import Card from './Card';
+import Input from './Input';
+import NumberContainer from './NumberContainer';
+import Colors from './constants/Colors'
 
 const StartGameScreen = props => {
-  const [enteredValue, setEnteredValue] = useState("");
+  const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
 
-  //If input is NOT a number (dot or commas), replace with empty value.
   const numberInputHandler = inputText => {
-    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
 
-  const deleteInputHandler = () => {
-    setEnteredValue("");
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
   };
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Your choice has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'cancel', onPress: resetInputHandler }]
+      );
+      return;
     }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue('');
+    Keyboard.dismiss();
   };
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+      </Card>
+    );
+  }
 
   return (
     <TouchableWithoutFeedback
@@ -37,28 +64,36 @@ const StartGameScreen = props => {
     >
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game!</Text>
-        <View style={styles.inputContainer}>
+        <Card style={styles.inputContainer}>
           <Text>Select a Number</Text>
-          <TextInput
-            style={styles.textInput}
-            keyboardType={"number-pad"}
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
             maxLength={2}
             onChangeText={numberInputHandler}
             value={enteredValue}
           />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
-              <Button title={"Reset"} onPress={deleteInputHandler} />
+              <Button
+                title="Reset"
+                onPress={resetInputHandler}
+                color={Colors.accent}
+              />
             </View>
             <View style={styles.button}>
               <Button
-                title={"Confirm"}
+                title="Confirm"
                 onPress={confirmInputHandler}
-                color="#4BB543"
+                color={Colors.primary}
               />
             </View>
           </View>
-        </View>
+        </Card>
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -68,48 +103,33 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: "center"
+    alignItems: 'center'
   },
-
   title: {
     fontSize: 20,
     marginVertical: 10
   },
-
   inputContainer: {
     width: 300,
-    minWidth: "80%",
-    alignItems: "center",
-    // Shadow Properties only works on IOS
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    // Elevation works in the same way as Shadow Properties but it hass less control.
-    elevation: 8,
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 10
+    maxWidth: '80%',
+    alignItems: 'center'
   },
-
-  textInput: {
-    width: "25%",
-    textAlign: "center",
-    fontSize: 25,
-    borderWidth: 1,
-    borderBottomColor: "grey",
-    marginVertical: 15
-  },
-
   buttonContainer: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-evenly",
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
     paddingHorizontal: 15
   },
-
   button: {
-    width: "40%"
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   }
 });
 
--- Components/GuessTheNumber.js
+++ Components/GuessTheNumber.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from 'react-native';
+
 import Header from './GTN/Header'
 import StartGameScreen from './GTN/StartGameScreen';
+import GameScreen from './GTN/GameScreen'
 
 
 export default function GuessTheNumber() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen />;
+
+  }
+
   return (
     <View style={styles.container}>
       <Header title={'Guess The Number'} />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
